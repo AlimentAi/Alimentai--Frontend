87fd306fcb21d4cefcce9b46dae4b8e51fcdf172
José Leandro <leandro.jlds@gmail.com>
2024-04-24 00:05:36 -0300

    Adiciona Searchbar em Categorias

--- src/components/categorias/ListaCategoria.tsx
+++ src/components/categorias/ListaCategoria.tsx
@@ -9,7 +9,7 @@ import CardCategoria from './CardCategoria';
 function ListaCategoria() {
   const [categorias, setCategorias] = useState<Categoria[]>([]);
   const [categoriaSelecionada, setCategoriaSelecionada] = useState<string>("");
-
+  const [filtroCategoria, setFiltroCategoria] = useState<string>("");
 
   let navigate = useNavigate();
 
@@ -43,6 +43,14 @@ function ListaCategoria() {
   const handleSelecionarCategoria = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCategoriaSelecionada(e.target.value);
   };
+
+  const handleFiltrarCategorias = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFiltroCategoria(e.target.value);
+  };
+
+  const categoriasFiltradas = categorias.filter(categoria =>
+    categoria.nome.toLowerCase().includes(filtroCategoria.toLowerCase())
+  );
   
   return (
     <>
@@ -59,32 +67,39 @@ function ListaCategoria() {
         </div>
       )}
 
-<div className="flex justify-center w-full my-4">
-  <div className="container flex flex-col">
-  <span style={{ fontFamily: 'Roboto', fontWeight: 'normal' }} className='font-semibold text-gray-800 self-start py-3 mx-20'>COMPRE POR</span>
-    <div className="bg-green-100 text-gray-800 py-7 px-10 mb-20 flex items-center rounded-full mx-10">
-      <span style={{ fontFamily: 'Roboto', fontWeight: 'normal' }} className='font-semibold mr-4'>CATEGORIAS</span>
-      <select
-        name="categoria"
-        id="categoria"
-        value={categoriaSelecionada}
-        onChange={handleSelecionarCategoria}
-        className='border-slate-800 rounded bg-transparent ml-20'
-      >
-        <option value="" disabled>Selecione uma categoria</option>
-        {categorias.map((categoria) => (
-          <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
-        ))}
-      </select>
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {categorias.map((categoria) => ( 
-        <CardCategoria key={categoria.id} categoria={categoria} />
-      ))}
-    </div>
-  </div>
-</div>
-
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col">
+          <span style={{ fontFamily: 'Roboto', fontWeight: 'normal' }} className='font-semibold text-gray-800 self-start py-3 mx-20'>COMPRE POR</span>
+          <div className="bg-green-100 text-gray-800 py-7 px-10 mb-20 flex items-center rounded-full mx-10">
+            <span style={{ fontFamily: 'Roboto', fontWeight: 'normal' }} className='font-semibold mr-4'>CATEGORIAS</span>
+            <select
+              name="categoria"
+              id="categoria"
+              value={categoriaSelecionada}
+              onChange={handleSelecionarCategoria}
+              className='border-slate-800 rounded bg-transparent ml-20'
+            >
+              <option value="" disabled>Selecione uma categoria</option>
+              {categorias.map((categoria) => (
+                <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
+              ))}
+            </select>
+            <input
+              type="text"
+              placeholder="Pesquisar por categoria"
+              value={filtroCategoria}
+              onChange={handleFiltrarCategorias}
+              className='border-slate-800 rounded bg-white ml-auto px-3 py-1 ml-200'
+              style={{ marginRight: '20px', width: '400px' }}
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {categoriasFiltradas.map((categoria) => ( 
+              <CardCategoria key={categoria.id} categoria={categoria} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
