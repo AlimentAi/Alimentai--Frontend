87cc7c0ef7c715afabc595d1aaf27183b693af09
Joao-VPS <joaovictorpuglessabino@gmail.com>
2024-04-24 09:11:03 -0300

    Alterações de estilo na home para melhor visualização dos componentes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
         
 
       <div className='flex justify-center border-b-4 border-neutral-200 top-8 relative pb-5'>
-        <Carousel transition={{ duration: 2 }} className="rounded-xl w-1/2 h-1/2" autoplay={true}>
+        <Carousel transition={{ duration: 2 }} className="rounded-xl w-1/2 h-96" autoplay={true} loop={true} >
           <img
             src={foto1}
             alt="image 1"
@@ -35,8 +35,8 @@ function Home() {
       <div>
         <div className='top-12 relative'><h1 className='text-3xl'> Sobre o ALIMENTAi </h1></div>
 
-        <div className='flex flex-col flex-center mx-[20%] my-10 m-auto min-h-[62vh] text-justify top-8 relative'>
-          <p className='text-1xl flex '>Bem-vindo à nossa plataforma dedicada a conectar pequenos produtores e agricultores com consumidores conscientes, apaixonados por produtos frescos e de qualidade. Aqui na nossa comunidade, valorizamos não apenas os alimentos que você compra, mas também as histórias por trás deles - histórias de trabalho árduo, dedicação à terra e um compromisso com práticas sustentáveis.</p>
+        <div className='flex flex-col flex-center mx-[20%] my-10 m-auto min-h-[62vh] text-justify top-8 relative gap-4'>
+          <p className='text-1xl'>Bem-vindo à nossa plataforma dedicada a conectar pequenos produtores e agricultores com consumidores conscientes, apaixonados por produtos frescos e de qualidade. Aqui na nossa comunidade, valorizamos não apenas os alimentos que você compra, mas também as histórias por trás deles - histórias de trabalho árduo, dedicação à terra e um compromisso com práticas sustentáveis.</p>
 
           <p className='text-1xl'> Nosso propósito é criar uma ponte entre os que cultivam e os que consomem, promovendo uma conexão genuína e transparente. Acreditamos que cada produto tem uma história única, e é essa narrativa que queremos compartilhar com você, nosso estimado cliente.</p>
 
@@ -47,7 +47,7 @@ function Home() {
           <p className='text-1xl'>Junte-se a nós nessa jornada de descoberta e celebração da agricultura local. Juntos, podemos fazer uma diferença real, apoiando pequenos produtores e criando um futuro mais sustentável para todos.</p>
         </div>
 
-        <div className='mb-8'>
+        <div className='my-8 mt-16'>
           <p className='text-1xl'>Obrigado por fazer parte da nossa comunidade.</p>
 
           <p className='text-1xl'>Atenciosamente,</p>
