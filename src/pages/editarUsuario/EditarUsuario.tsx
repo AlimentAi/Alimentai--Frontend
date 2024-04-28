73bc165cc61201dee13227182e2321d4f07f8e91
Joao-VPS <joaovictorpuglessabino@gmail.com>
2024-04-28 16:16:18 -0300

    Melhorado a página do usuário. Adicionado foto do RootUser.

--- src/pages/editarUsuario/EditarUsuario.tsx
+++ src/pages/editarUsuario/EditarUsuario.tsx
@@ -4,27 +4,74 @@ import { Link, useNavigate } from 'react-router-dom'
 import { toastAlerta } from '../../utils/toastAlerta'
 import { atualizar } from '../../services/Service'
 import Usuario from '../../models/Usuario'
-import { Basket, CaretLeft, Carrot } from '@phosphor-icons/react'
+import { Basket, CaretLeft, Carrot, ImageBroken, PencilSimpleLine, Trash, UserPlus } from '@phosphor-icons/react'
 import Logo from "../../assets/logo.png"
+import { toast } from 'react-toastify'
 
 function EditarUsuario() {
-  const { usuario, handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout, handleLogin } = useContext(AuthContext)
   let navigate = useNavigate()
 
-  const [novoUsuario, setNovoUsuario] = useState({} as Usuario)
+  const [novoUsuario, setNovoUsuario] = useState({ ...usuario })
+  const [senhaValida, setSenhaValida] = useState(false)
+  const [confirmarSenha, setConfirmarSenha] = useState("")
 
   async function atualizarUsuario(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault()
 
-    try {
-      await atualizar('/usuarios', novoUsuario, setNovoUsuario, {
-        headers: {
-          Authorization: usuario.token
-        }
-      })
-    } catch (error) {
-      toastAlerta('Falha ao atualizar usuário', 'error')
-      console.log(error)
+    if (confirmarSenha === novoUsuario.senha &&
+      senhaValida &&
+      (novoUsuario.tipo === 'consumidor' || novoUsuario.tipo === 'produtor' || novoUsuario.tipo === 'administrador')) {
+      
+      try {
+        await atualizar('/usuarios/atualizar', novoUsuario, setNovoUsuario, {
+          headers: {Authorization: usuario.token}
+        })
+        const mensagemSucesso = (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <span className="font-semibold">Usuário atualizado com sucesso</span>
+            <img src="src/assets/beteration/beterraba-cadastro-success.png" alt="Usuário Cadastrado com Sucesso" style={{ width: '100px', height: '100px', marginTop: '8px' }} />
+          </div>
+        );
+        toast.success(mensagemSucesso);
+
+        handleLogin(novoUsuario)
+      } catch (error) {
+        const mensagemErro = (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <span className="font-semibold">Erro ao cadastrar o Usuário</span>
+            <img src="https://i.imgur.com/F1Yn7v3.png" alt="Erro ao Cadastrar Usuário" style={{ width: '100px', height: '100px', marginTop: '5px' }} />
+          </div>
+        );
+        toast.error(mensagemErro);
+        console.log(error)
+      }
+    }
+    
+    switch (false) {
+      case confirmarSenha === novoUsuario.senha:
+        const mensagemSenhaInconsistente = (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <span className="font-semibold">Dados inconsistentes, as senhas precisam ser iguais.</span>
+            <img src="https://i.imgur.com/aAwsVDm.png" alt="Dados Inconsistentes" style={{ width: '100px', height: '100px', marginTop: '2px' }} />
+          </div>
+        );
+        toast.info(mensagemSenhaInconsistente);
+        setNovoUsuario({ ...novoUsuario, senha: "" }) // Reinicia o campo de Senha
+        setConfirmarSenha("")                 // Reinicia o campo de Confirmar Senha
+        break
+      case senhaValida:
+        const mensagemSenhaFraca = (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <span className="font-semibold">A senha não atende aos requisitos mínimos.</span>
+            <img src="https://i.imgur.com/F1Yn7v3.png" alt="Senha Fraca" style={{ width: '100px', height: '100px', marginTop: '5px' }} />
+          </div>
+        );
+        toast.info(mensagemSenhaFraca);
+        break
+      case usuario.tipo === 'consumidor' || usuario.tipo === 'produtor' || usuario.tipo === 'administrador':
+        toast.info('Informe se você é um consumidor ou produtor')
+        break
     }
   }
 
@@ -53,7 +100,7 @@ function EditarUsuario() {
   function ocultarRequisitos() {
     const lista = document.querySelector("#requisitos-lista")
 
-    if (usuario.senha.length === 0) {
+    if (novoUsuario.senha.length === 0) {
       lista?.classList.remove("block")
       lista?.classList.add("hidden")
     }
@@ -138,26 +185,33 @@ function EditarUsuario() {
     }
   }
 
-  function handleConfirmarSenha(e: React.ChangeEvent<HTMLInputElement>): void {
-    throw new Error('Function not implemented.')
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value)
+  }
+
+  function limparImagem() {
+    setNovoUsuario({
+      ...novoUsuario,
+      foto: 'https://i.imgur.com/Tk9f10K.png'
+    })
   }
 
   return (
-    <div className="w-full flex flex-col pt-8 pb-16">
-      <div className="justify-center flex items-center">
-        <h1 className="text-3xl font-bold">ATUALIZAR DADOS</h1>
+    <div className="w-full max-w-5xl items-center flex flex-col pt-8 pb-16 px-16 mx-auto">
+      <div className="flex items-center">
+        <p className="text-3xl font-bold">ATUALIZAR DADOS</p>
         <img src={Logo} className="w-14" alt="" />
       </div>
-      <p className="m-4 font-bold">Crie sua vitrine online e ganhe visibilidade de milhares de clientes</p>
-      <div className="h-full flex flex-col items-start p-4">
-        <form className="w-full flex flex-wrap justify-center items-center gap-4" onSubmit={atualizarUsuario}>
-          <div className="w-[80%] flex flex-col justify-center items-center gap-4">
-            <div className="w-[90%] flex flex-col items-start gap-4">
+      <form className="h-full w-full flex flex-col p-4" onSubmit={atualizarUsuario}>
+        <div className="w-full flex gap-4">
+          <div className="w-full flex flex-col justify-between items-center gap-4">
+            <div className="w-full flex flex-col items-start gap-4">
               <h2 className="text-2xl font-semibold">Dados de contato</h2>
               <input type="text"
                 className="w-full border-2 rounded-lg p-3 border-[#cfcccc] hover:border-[#c42342] duration-1000 dark:bg-zinc-800"
                 placeholder="Seu nome / Nome da empresa"
                 name="nome"
+                value={novoUsuario.nome}
                 required
                 onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
               <input
@@ -165,16 +219,17 @@ function EditarUsuario() {
                 className="w-full border-2 rounded-lg p-3 border-[#cfcccc] hover:border-[#c42342] duration-1000 dark:bg-zinc-800"
                 placeholder="E-mail"
                 name="email"
+                value={novoUsuario.email}
                 required
                 onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)} />
             </div>
 
-            <div className="w-[90%] flex flex-col items-start gap-4">
+            <div className="w-full flex flex-col items-start gap-4">
               <h2 className="text-2xl font-semibold">Dados de acesso</h2>
               <input
                 type="password"
                 className="w-full border-2 rounded-lg p-3 border-[#cfcccc] hover:border-[#c42342] duration-1000 dark:bg-zinc-800"
-                placeholder="Sua senha"
+                placeholder="Digite uma nova senha ou sua senha atual"
                 name="senha"
                 onFocus={mostrarRequisitos}
                 onBlur={ocultarRequisitos}
@@ -195,56 +250,96 @@ function EditarUsuario() {
                 <li id="tem-numero">Um número</li>
                 <li id="sem-espaco">Não pode conter espaços.</li>
               </ul>
-              <div className="flex w-full justify-around flex-wrap my-4">
-                <div className={`${novoUsuario.tipo === 'consumidor' ? 'text-green-600 dark:text-green-400' : ''} duration-300`}>
-                  <input
-                    id="consumidor"
-                    type="radio"
-                    name="tipo"
-                    className="invisible"
-                    value="consumidor"
-                    onChange={
-                      (e: ChangeEvent<HTMLInputElement>) => {
-                        if (e.target.checked) atualizarEstado(e);
-                      }
-                    } />
-                  <label htmlFor="consumidor" className="flex flex-col items-center">
-                    <Basket size={42} />
-                    Sou consumidor
-                  </label>
-                </div>
-                <div className={`${novoUsuario.tipo === 'produtor' ? 'text-orange-600 dark:text-orange-400' : ''} duration-300`}>
-                  <input
-                    id="produtor"
-                    type="radio"
-                    name="tipo"
-                    className="invisible"
-                    value="produtor"
-                    onChange={
-                      (e: ChangeEvent<HTMLInputElement>) => {
-                        if (e.target.checked) atualizarEstado(e)
-                      }
-                    } />
-                  <label htmlFor="produtor" className="flex flex-col items-center">
-                    <Carrot size={42} />
-                    Sou produtor
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div className="w-[90%] mt-4 flex items-center justify-center gap-4">
-              <Link to='/'>
-                <button className="w-full pr-4 h-14 flex items-center gap-1 p-1 rounded-lg text-[#f76c6f] hover:bg-[#c42342] hover:text-white font-bold duration-1000">
-                  <CaretLeft size={40} />
-                  <span className="">Cancelar</span>
-                </button>
-              </Link>
-              <button type='submit' className="w-[50%] h-14 bg-[#f76c6f] rounded-lg font-bold text-white hover:bg-[#c42342] duration-1000">Atualizar dados</button>
             </div>
           </div>
-        </form>
-      </div>
+          <div className='h-full w-full flex flex-col justify-center items-center gap-4'>
+            <p className="text-3xl font-bold">Imagem do usuário</p>
+            <div className='w-auto h-full max-w-64 max-h-64 overflow-hidden border p-2 rounded-lg'>
+              {novoUsuario.foto.length < 10 || !(novoUsuario.foto.includes('.png') || novoUsuario.foto.includes('.jpg')) || novoUsuario.foto === null ?
+                <ImageBroken className='w-full h-auto object-cover' size={256}/> :
+                <img src={novoUsuario.foto} alt={`Esta é sua foto, ${novoUsuario.nome}`} />}
+            </div>
+            <div className='flex gap-4'>
+              <input
+                id='link-imagem'
+                type="text"
+                name='foto'
+                className='border rounded-lg text-black appearance-none'
+                value={novoUsuario.foto}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                placeholder='Digite o link da foto desejada' />
+              <button type='button' onClick={limparImagem} className='border flex flex-col items-center p-2 rounded-lg'>Remover imagem <Trash /></button>
+            </div>
+          </div>
+          
+        </div>
+        <div className="flex justify-around my-4">
+          <div className={`${novoUsuario.tipo === 'consumidor' ? 'text-green-600 dark:text-green-400' : ''} duration-300`}>
+            <input
+              id="consumidor"
+              type="radio"
+              name="tipo"
+              className="invisible"
+              value="consumidor"
+              defaultChecked={usuario.tipo === 'consumidor'}
+              onChange={
+                (e: ChangeEvent<HTMLInputElement>) => {
+                  if (e.target.checked) atualizarEstado(e);
+                }
+              } />
+            <label htmlFor="consumidor" className="flex flex-col items-center">
+              <Basket size={42} />
+              Sou consumidor
+            </label>
+          </div>
+          <div className={`${novoUsuario.tipo === 'produtor' ? 'text-orange-600 dark:text-orange-400' : ''} duration-300`}>
+            <input
+              id="produtor"
+              type="radio"
+              name="tipo"
+              className="invisible"
+              value="produtor"
+              defaultChecked={usuario.tipo === 'produtor'}
+              onChange={
+                (e: ChangeEvent<HTMLInputElement>) => {
+                  if (e.target.checked) atualizarEstado(e)
+                }
+              } />
+            <label htmlFor="produtor" className="flex flex-col items-center">
+              <Carrot size={42} />
+              Sou produtor
+            </label>
+          </div>
+          {usuario.tipo === 'administrador' &&
+            <div className={`${novoUsuario.tipo === 'administrador' ? 'text-red-600 dark:text-red-400' : ''} duration-300`}>
+              <input
+                id="administrador"
+                type="radio"
+                name="tipo"
+                className="invisible"
+                value="administrador"
+                defaultChecked={usuario.tipo === 'administrador'}
+                onChange={
+                  (e: ChangeEvent<HTMLInputElement>) => {
+                    if (e.target.checked) atualizarEstado(e)
+                  }
+                } />
+              <label htmlFor="administrador" className="flex flex-col items-center">
+                <UserPlus size={42} />
+                Sou administrador
+              </label>
+            </div>}
+        </div>
+        <div className="mt-4 flex items-center justify-center gap-4">
+          <Link to='/'>
+            <button className="w-full pr-4 h-14 flex items-center gap-1 p-1 rounded-lg text-[#f76c6f] hover:bg-[#c42342] hover:text-white font-bold duration-1000">
+              <CaretLeft size={40} />
+              <span className="">Cancelar</span>
+            </button>
+          </Link>
+          <button type='submit' className="w-[50%] h-14 bg-[#f76c6f] rounded-lg font-bold text-white hover:bg-[#c42342] duration-1000">Atualizar dados</button>
+        </div>
+      </form>
     </div>
   )
 }
