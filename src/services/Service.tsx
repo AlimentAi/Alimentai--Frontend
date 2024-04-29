a05c51f738569d995fcde62992c76b7b68bd2349
José Leandro <leandro.jlds@gmail.com>
2024-04-29 00:16:56 -0300

    Service fix

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: 'https://generation-pi-ecommerce.onrender.com'
     //baseURL: 'http://localhost:8080/' //Rota para restes locais
 })
 
